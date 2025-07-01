ca1b3ff4a4ce2bc7ba1c35cd3afdfc088352f72b
anuplohar001 <anuplohar001@gmail.com>
2025-07-01 18:59:37 +0530

    add: collections

--- terraform/main.tf
+++ terraform/main.tf
@@ -38,6 +38,14 @@ resource "aws_security_group" "existing_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "Custom TCP"
+    from_port = 27017
+    to_port = 27017
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   ingress  {
     description = "SSH"
     from_port = 22
